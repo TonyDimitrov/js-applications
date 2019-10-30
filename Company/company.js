37616161e55cd941233850a5a31d1514eb59d1bd
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-10-30 08:08:26 +0200

    this exercise

---
+++ Company/company.js
@@ -0,0 +1,14 @@
+class Company{
+    
+    constructor(){
+        this._departments;
+    }
+
+    get departments(){
+        return this._departments;
+    }
+
+    addEmployee(username, salary, position, department) {
+        return 5;
+    }
+}
