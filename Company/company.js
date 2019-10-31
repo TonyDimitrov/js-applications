17fc0cec7b8d6156ada23848c9d55a0999e735d2
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-10-31 08:52:51 +0200

    Company class

--- Company/company.js
+++ Company/company.js
@@ -1,14 +1,35 @@
-class Company{
-    
-    constructor(){
-        this._departments;
+class Company {
+
+    constructor() {
+        this._departments = [];
     }
 
-    get departments(){
+    get departments() {
         return this._departments;
     }
 
+    set departments(employee) {
+        this._departments.push(employee);
+    }
+
+
     addEmployee(username, salary, position, department) {
-        return 5;
+        if (username === "" || username === null || username === undefined
+            || salary < 0
+            || position === "" || position === null || position === undefined
+            || department === "" || department === null || department === undefined) {
+            throw new Error("Invalid input!");
+        }
+        let empDepartment = {
+            username,
+            salary,
+            position,
+            department
+        };
+        this.departments.push(empDepartment);
+        return `New employee is hired. Name: ${username}. Position: ${position}`;
     }
 }
+
+let cmp = new Company();
+cmp.addEmployee("Stanimir", -2000, "engineer", "Construction");
