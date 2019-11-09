a628390295a9787b858c919dbc82ff32001589fb
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-11-09 11:38:50 +0200

    js application

--- Company/company.js
+++ Company/company.js
@@ -1,10 +1,10 @@
-class Company{
-    
-    constructor(){
+class Company {
+
+    constructor() {
         this._departments;
     }
 
-    get departments(){
+    get departments() {
         return this._departments;
     }
 
@@ -12,3 +12,20 @@ class Company{
         return 5;
     }
 }
+
+function solve(a, b) {
+    let newObj = Object.create(a, {
+        address: {
+            writable: true,
+            configurable: true,
+            enumerable: true,
+            value: "Sofia"
+        }
+    });
+    console.log(newObj.age);
+}
+
+let obj1 = { name: "Toni", age: 25 }
+
+
+solve(obj1);
