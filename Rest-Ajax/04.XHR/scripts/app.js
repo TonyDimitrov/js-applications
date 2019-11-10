81223a40e225ff9e39bf4d3d4271d6ade13b3278
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-11-10 17:20:57 +0200

    XMLHttpRequest and fetch exercises

--- Rest-Ajax/04.XHR/scripts/app.js
+++ Rest-Ajax/04.XHR/scripts/app.js
@@ -1,3 +1,36 @@
-function loadRepos() {
-   console.log("TODO...");
+// (function loadRepos() {
+   
+//    let btn = document.querySelector("body > button");
+//    const url = "https://api.github.com/repos/testnakov/test-nakov-repo";
+
+//    btn.addEventListener('click', getRepoIssues);
+
+//    function getRepoIssues(){
+
+//       let httpRequest = new XMLHttpRequest();
+    
+//       httpRequest.addEventListener('readystatechange',   function(){
+//          if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+//             document.getElementById("res").innerText = httpRequest.responseText;
+//          } else{
+//             // log error
+//             console.log("Error " + httpRequest.readyState);
+//          }
+//        });
+//        httpRequest.open('GET', url);
+//        httpRequest.send();    
+//    }
+// })()
+
+function fetchRepos(){
+   let btn = document.querySelector("body > button");
+   const url = "https://api.github.com/repos/testnakov/test-nakov-repo";
+   btn.addEventListener('click', getRepoIssues);
+
+   function getRepoIssues(){
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => document.getElementById("res").innerText = JSON.stringify(data))
+        .catch((error) => console.error(error))
+   }
 }
--- Rest-Ajax/05.Github-Repos/scripts/app.js
+++ Rest-Ajax/05.Github-Repos/scripts/app.js
@@ -1,3 +1,58 @@
 function loadRepos() {
-	console.log("TODO...");
+
+	let btn = document.querySelector("body>button");
+
+	btn.addEventListener("click", btnHandler);
+
+	function btnHandler() {
+
+		let input = document.querySelector("body>input");
+		let username = input.value;
+		if (username != "") {
+		    repoCaller(username);
+			input.value = "";
+		}
+	}
+	function repoCaller(repoUser) {
+		let url = `https://api.github.com/users/${repoUser}/repos`;
+
+		fetch(url)
+			.then((response) => {
+				if (response.status === 200) {
+					displayRepos(response.json());
+				} else {
+					displayRepos(undefined, repoUser);
+					throw new Error(`No repositories for ${repoUser} found. Server returned: `  + response.status);
+				}
+			})
+			.catch(error => { console.error(error) });
+	}
+
+	function displayRepos(promise, repoUser) {
+
+		let ul = document.querySelector("#repos");
+		ul.innerHTML = "";
+
+		if (promise) {
+			promise.then((data) => data.map(arr => {
+				let repo = {};
+				repo.fullName = arr.full_name,
+					repo.htmlUrl = arr.html_url
+				return repo;
+			})).then((filteredData) => {
+				for (const repo of filteredData) {
+					let li = document.createElement("li");
+					let a = document.createElement("a");
+					a.href = repo.htmlUrl;
+					a.innerText = repo.fullName;
+					li.appendChild(a);
+					ul.appendChild(li);
+				}
+			})		
+		} else {
+			let li = document.createElement("li");
+			li.innerText = `No repositories for user '${repoUser}' found!`;
+			ul.appendChild(li);
+		}
+	}
 }
