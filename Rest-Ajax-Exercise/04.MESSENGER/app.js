a7ad2bdc602890d02e39952f3d2d8f73e62f22a0
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-11-17 01:19:40 +0200

    Messanger

--- Rest-Ajax-Exercise/04.MESSENGER/app.js
+++ Rest-Ajax-Exercise/04.MESSENGER/app.js
@@ -1,5 +1,49 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    let btnRefresh = document.getElementById("refresh");
+    btnRefresh.addEventListener("click", getMessages);
+
+    let btnSubmit = document.getElementById("submit");
+    btnSubmit.addEventListener("click", addMessage);
+
+    async function getMessages() {
+        let messages = await fetch("https://rest-messanger.firebaseio.com/messanger.json")
+            .then(r => r.json());
+        displayMessage(messages);
+    }
+
+    async function addMessage() {
+
+        let impAuthor = document.getElementById("author");
+        let impContent = document.getElementById("content");
+
+        if (impAuthor.value && impContent.value) {
+
+            let postBody = {};
+            postBody.author = impAuthor.value;
+            postBody.content = impContent.value;
+
+            let messages = await fetch("https://rest-messanger.firebaseio.com/messanger.json",
+                {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(postBody)
+                })
+                .then(r => r.json());
+            impAuthor.value = "";
+            impContent.value = "";
+            getMessages();
+        }
+    }
+
+    function displayMessage(messages) {
+        let msgContent = Object.keys(messages).map(e => { let inrMsg = messages[e]; return `${inrMsg.author}: ${inrMsg.content}` });
+        let concatMsg = msgContent.join("&#13;&#10;");
+        let textElement = document.getElementById("messages");
+        textElement.innerHTML = concatMsg;
+    }
 }
 
 attachEvents();
